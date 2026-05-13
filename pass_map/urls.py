81dd2d2ed6b029a9eea78e0789433f59c746cbd5
Stukalov MN <ya@mstukalov.ru>
2026-05-13 13:43:35 +0300

    Passport Editor v0.15.2

--- pass_map/urls.py
+++ pass_map/urls.py
@@ -15,12 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from pass_viewer.forms import RussianAuthenticationForm
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'accounts/login/',
+        auth_views.LoginView.as_view(authentication_form=RussianAuthenticationForm),
+        name='login',
+    ),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('pass_viewer.urls')),
 ]
